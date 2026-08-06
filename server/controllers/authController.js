361c6d44edb38545e0ce186874da77e4d83c40f7
Harshit Kumar <hr171004@gmail.com>
2026-08-06 13:31:17 +0530

    Fix Nodemailer ETIMEDOUT by forcing port 587 and STARTTLS

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -193,7 +193,9 @@ const forgotPassword = async (req, res) => {
 
     // Send email
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // Use STARTTLS on port 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
